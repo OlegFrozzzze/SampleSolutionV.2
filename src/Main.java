9d7e0a014685443744360f83ec9a0b7a63ddd212
Frozzzze <ji900@yandex.ru>
2017-06-29 02:45:17 +0300

    Добавлен новый метод отвечающий за ввод данных с клавиатуры.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created by ОЛЕГ on 28.06.2017.
  */
@@ -9,5 +11,10 @@ public class Main {
         // Выбор из меню нужное решение
         // Вывод выброного решения на экран
 
+    }
+    public  static int getUserChoise(){
+        System.out.println("Введите номер выбранного варианта");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
     }
 }
