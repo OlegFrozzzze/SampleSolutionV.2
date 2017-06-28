c0db6a951ae94710b267d8ca5492bb4f3b9cbd02
Frozzzze <ji900@yandex.ru>
2017-06-28 16:32:51 +0300

    Дабвлены 2 примера

--- src/SampleSolution.java
+++ src/SampleSolution.java
@@ -3,21 +3,32 @@
  */
 public class SampleSolution {
     // Создать 2 метода с решением задачи
-    public static void solutionOne(){
+    public static void solutionOne() {
         //Пример
+        for (int i = 1; i < 30; i++) {
+            System.out.println(i + "+" + (i + 1) + "=" + (i + (i + 1)));
+        }
     }
-    public static void solutionTwo(){
-        //Пример
+
+    public static void solutionTwo() {
+        for (int i = 1; i < 30; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i + "+" + (i + 1) + "=" + (i + (i + 1)));
+            }
+        }
     }
+
     // Создать меню для выбора нужного варианта
-    public static void menuShow(){
-     //Текст меню
+    public static void menuShow() {
+        //Текст меню
     }
+
     // Создать методы для вывода результата в файлы txt и html
-    public static void sampleSolutionInTxtFile(){
-        //создать объект куда мы записываем решение примера 
+    public static void sampleSolutionInTxtFile() {
+        //создать объект куда мы записываем решение примера
     }
-    public static void sampleSolutionInHtmlFile(){
+
+    public static void sampleSolutionInHtmlFile() {
         //создать объект куда мы записываем решение примера
     }
 }
