ce986754b10a33d69c91eb19012b601c5d581b7d
Frozzzze <ji900@yandex.ru>
2017-06-28 16:17:43 +0300

    Созданы новые методы

--- src/SampleSolution.java
+++ src/SampleSolution.java
@@ -3,6 +3,15 @@
  */
 public class SampleSolution {
     // Создать 2 метода с решением задачи
+    public static void solutionOne(){
+        //Пример
+    }
+    public static void solutionTwo(){
+        //Пример
+    }
     // Создать меню для выбора нужного варианта
+    public static void menuShow(){
+     //Текст меню
+    }
     // Создать метод для вывода результата в файлы txt и html
 }
