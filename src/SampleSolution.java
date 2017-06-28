140dbfcead9b63c1c4c6fe12473a873f8561defc
Frozzzze <ji900@yandex.ru>
2017-06-28 21:18:51 +0300

    Изменины названия массивов Добавлен текст меню

--- src/SampleSolution.java
+++ src/SampleSolution.java
@@ -3,14 +3,15 @@
  */
 public class SampleSolution {
     // Создать 2 метода с решением задачи
-    public static void solutionOne() {
+    public static void sampleOne() {
         //Пример
         for (int i = 1; i < 30; i++) {
             System.out.println(i + "+" + (i + 1) + "=" + (i + (i + 1)));
         }
     }
 
-    public static void solutionTwo() {
+    public static void sampleTwo() {
+        //Пример
         for (int i = 1; i < 30; i++) {
             if (i % 2 != 0) {
                 System.out.println(i + "+" + (i + 1) + "=" + (i + (i + 1)));
@@ -21,14 +22,21 @@ public class SampleSolution {
     // Создать меню для выбора нужного варианта
     public static void menuShow() {
         //Текст меню
+        System.out.println("Выберите один вариантов");
+        System.out.println("1) Последовательное попарное сложение чисел от 1 до 30");
+        System.out.println("2) Последовательное сложение чисел через один");
+        System.out.println("3) Вывод последовательного попарного сложения чисел от 1 до 30 в txt файл");
+        System.out.println("4) Вывод последовательного сложения чисел через один в txt файл");
+        System.out.println("5) Вывод последовательного попарного сложения чисел от 1 до 30 в html файл");
+        System.out.println("6) Вывод последовательного сложения чисел через один в html файл");
     }
 
     // Создать методы для вывода результата в файлы txt и html
-    public static void sampleSolutionInTxtFile() {
+    public static void sampleSolutionToTxtFile() {
         //создать объект куда мы записываем решение примера
     }
 
-    public static void sampleSolutionInHtmlFile() {
+    public static void sampleSolutionToHtmlFile() {
         //создать объект куда мы записываем решение примера
     }
 }
