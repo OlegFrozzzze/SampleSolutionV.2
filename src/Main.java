4801f918f22c759fec893cd49021a3c1955ba81b
Frozzzze <ji900@yandex.ru>
2017-06-29 03:02:35 +0300

    Добавлено пункты меню

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,32 @@ public class Main {
         // Вывод меню
         SampleSolution.menuShow();
         // Выбор из меню нужное решение
-        // Вывод выброного решения на экран
-
+        int variant = SampleSolution.getUserChoise();
+        if (variant == 1) {
+            SampleSolution.sampleOne();
+        } else {
+            if (variant == 2){
+                SampleSolution.sampleTwo();
+            }else{
+                if(variant == 3){
+                    SampleSolution.sampleSolutionToTxtFileSampleOne();
+                }else{
+                    if(variant == 4){
+                        SampleSolution.sampleSolutionToTxtFileSampleTwo();
+                    }else{
+                        if(variant == 5){
+                            SampleSolution.sampleSolutionToHtmlFileSampleOne();
+                        }else{
+                            if(variant == 6){
+                                SampleSolution.sampleSolutionToHtmlFileSampleTwo();
+                            }else{
+                                System.out.println("Введено неверное число");
+                                System.out.println("Перезапустите программу");
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
